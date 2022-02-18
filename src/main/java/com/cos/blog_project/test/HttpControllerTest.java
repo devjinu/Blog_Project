13124613513eedc430782beb7888404ba5b4f38b
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-02-18 17:21:05 +0900

    lombok test

--- src/main/java/com/cos/blog_project/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog_project/test/HttpControllerTest.java
@@ -12,27 +12,38 @@ public class HttpControllerTest {
     // body에서 데이터를 보내면, 오브젝트로 매핑해서 받을 수 있다(@RequestBody)
     // messageconverter(스프링부트) -> 파싱해줌
 
+    private static final String tag = "HttpControllerTest : ";
+
+    @GetMapping("/http/lombok")
+    public String lombokTest() {
+        Member m = Member.builder().username("ssar").password("1234").email("ssar@naver.com").build();
+        System.out.println(tag + "getter : " + m.getUsername());
+        m.setUsername("cos");
+        System.out.println(tag + "setter :" + m.getUsername());
+        return "lombok test 완료";
+    }
+
     // http://localhost:8080/http/get (select)
     @GetMapping("/http/get")
-    public String getTest(Member m){
-        return "get 요청 : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+    public String getTest(Member m) {
+        return "get 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
 
     // http://localhost:8080/http/post (insert)
     @PostMapping("/http/post")
-    public String postTest(@RequestBody Member m){
-        return "post 요청 : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+    public String postTest(@RequestBody Member m) {
+        return "post 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
 
     // http://localhost:8080/http/put (update)
     @PutMapping("/http/put")
-    public String putTest(@RequestBody Member m){
-        return "put 요청 : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+    public String putTest(@RequestBody Member m) {
+        return "put 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
 
     // http://localhost:8080/http/delete (delete)
     @DeleteMapping("/http/delete")
-    public String deleteTest(){
+    public String deleteTest() {
         return "delete 요청";
     }
 
--- src/main/java/com/cos/blog_project/test/Member.java
+++ src/main/java/com/cos/blog_project/test/Member.java
@@ -1,47 +1,28 @@
 package com.cos.blog_project.test;
 
+import lombok.*;
+
+/*@Getter
+@Setter*/
+
+@Data // -> getter and setter
+// @AllArgsConstructor  // -> constructor
+// @RequiredArgsConstructor // final 붙은 애들의 constructor 만들어줌
+@NoArgsConstructor // -> 빈 생성자
 public class Member {
+
     private int id;
     private String username;
     private String password;
     private String email;
 
+    // 생성자의 순서를 지키지 않아도 되고, 필드의 값을 신경쓰지 않아도 됨
+    @Builder
     public Member(int id, String username, String password, String email) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    
 }
