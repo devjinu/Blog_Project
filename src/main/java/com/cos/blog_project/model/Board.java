37ff4f08fd18a5399057fd724338be76a5915b29
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-02-21 20:43:36 +0900

    댓글 리스트 완료

--- src/main/java/com/cos/blog_project/model/Board.java
+++ src/main/java/com/cos/blog_project/model/Board.java
@@ -1,5 +1,6 @@
 package com.cos.blog_project.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -30,11 +31,12 @@ public class Board {
     private int count; // 조회수
 
     @ManyToOne(fetch = FetchType.EAGER) // Many = Board. User = One
-    @JoinColumn(name="userId")
+    @JoinColumn(name = "userId")
     private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
 
-    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다(fk x) DB에 컬럼생성 X
-    private List<Reply> reply;
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다(fk x) DB에 컬럼생성
+    @JsonIgnoreProperties({"board"}) // 무한참조 방지
+    private List<Reply> replys;
 
     @CreationTimestamp
     private Timestamp createDate;
---
+++ src/main/java/com/cos/blog_project/repository/ReplyRepository.java
@@ -0,0 +1,8 @@
+package com.cos.blog_project.repository;
+
+import com.cos.blog_project.model.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply, Integer> {
+
+}
